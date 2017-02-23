3aba5141d1c7d0334317f1f00452db5bb07b513e
madanagopal <madanagopal@gmail.com>
2017-02-23 17:38:22 -0500

    Add files via upload

---
+++ reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+	return [
+	{title : 'Java Script good parts'},
+	{title : 'Harry potter'},
+    {title : 'Java Script Bad'},
+    {title : 'Harry potter : Legacy'}
+	]
+}
